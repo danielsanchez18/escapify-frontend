11caa32e194330a7277d6f6241ba3e66d5e9643c
danielsanchez18 <dsanchez151r@gmail.com>
2025-09-18 12:14:25 -0500

    feat(prices): add detailed pricing plans for Free, Startup, Team, and Enterprise

--- src/app/components/customer/prices/plans/plans.component.ts
+++ src/app/components/customer/prices/plans/plans.component.ts
@@ -15,6 +15,31 @@ export class ComponentCustomerPricesPlans {
   readonly Check = Check;
   readonly MessagesSquare = MessagesSquare;
 
+  isAnnual: boolean = true;
+
+  readonly plans = [
+    {
+      name: 'Free',
+      price_monthly: 0,
+      price_annual: 0,
+    },
+    {
+      name: 'Startup',
+      price_monthly: 15,
+      price_annual: 30,
+    },
+    {
+      name: 'Team',
+      price_monthly: 45,
+      price_annual: 90,
+    },
+    {
+      name: 'Enterprise',
+      price_monthly: 199,
+      price_annual: 399,
+    },
+  ]
+
   features_free = [
     '5 GB Cloud Storage',
     '5 API Requests/min',
